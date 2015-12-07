5ba63eda396ee070368894473752e3d3ad3fa973
Sandy Place <splace@worksight.net>
2015-12-06 19:42:18 -0800

    scalable realtime io cluster.

---
+++ node/index.js
@@ -0,0 +1,21 @@
+var app  = require('express')();
+var http = require('http').Server(app);
+var io   = require('socket.io')(http);
+var redis = require('./redis-adapter.js');
+io.adapter(redis({ host: 'redis', port: 6379 }));
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  io.emit('sample', {"hey": "there"});
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+http.listen(8080, function(){
+  console.log('listening on *:8080');
+});
